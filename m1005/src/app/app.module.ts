bbacfad3b2a3510c4407387cc4da2187046460ab
Alejandro Romero <alejo8591@gmail.com>
2017-10-05 22:37:55 -0500

    Update app

--- m1005/src/app/app.module.ts
+++ m1005/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { DbServiceProvider } from '../providers/db-service/db-service';
+import { ProductServiceProvider } from '../providers/product-service/product-service';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DbServiceProvider,
+    ProductServiceProvider
   ]
 })
 export class AppModule {}
